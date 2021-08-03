444db48033da9b62d5c1742cb96117e53134b16f
Abdullah Khalid <66444446+AbdullahK047@users.noreply.github.com>
2021-08-04 03:26:54 +0500

    Add files via upload

--- init_pos_generate.py
+++ init_pos_generate.py
@@ -8,41 +8,19 @@ Created on Tue Jul 27 00:56:02 2021
 import random
 import numpy as np
 import math
-import matplotlib.pyplot as plt
 
 
-initial_ys = np.zeros(10)
+n = 100000
+initial_ys = np.zeros(n)
 
-for i in range(10):
+for i in range(n):
     initial_ys[i] = random.uniform(-5, 5)
-    #x.append(0)
-    
-print(initial_ys)
 
-plt.scatter([0,0,0,0,0,0,0,0,0,0], initial_ys)
+np.save("initial_ys", initial_ys)
 
-np.save("initial_ys2", initial_ys)
 
-"""
-initial_yaws = np.zeros(10)
-#x = []
-print(initial_yaws)
-
-for i in range(10):
+initial_yaws = np.zeros(n)
+for i in range(n):
     initial_yaws[i] = random.uniform(0, 2*math.pi)
-    #x.append(0)
-    
-print(initial_yaws)
 
 np.save("initial_yaws", initial_yaws)
-
-
-    
-plt.scatter(x, initial_ys)
-
-initial_ys = np.load("initial_ys.npy")
-initial_yaws = np.load("initial_yaws.npy")
-
-#plt.scatter([0,0,0,0,0,0,0,0,0,0], initial_ys)
-plt.scatter([0,0,0,0,0,0,0,0,0,0], initial_yaws, c="red")
-"""
